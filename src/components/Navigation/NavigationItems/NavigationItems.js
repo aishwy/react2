5078d365d4c3fc73f2e0b353d57a626a06e1ceed
aishwy <aishi.best@gmail.com>
2018-11-11 16:44:42 +0530

    navtoolbar

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
+import classes from './NavigationItems.css';
+const NavigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Buger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
+
+);
+export default NavigationItems;
