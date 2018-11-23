b82e73091819e0aad4dfcdb7de32585a7f6593d8
aishwy <aishi.best@gmail.com>
2018-11-23 10:34:50 +0530

    update

--- src/components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import logo from '../../assets/images/logo.png';
-import classes from './Logo.css';
-import React from 'react';
-const Logo = () => (
-    <div className={classes.Logo}>
-        <img src={logo} />
-    </div>
-);
-export default Logo;
